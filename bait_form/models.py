86b8d0b00adf8bdfa577c1e1ca3ab1067412a168
tiagofribeiro <tiagofr98@gmail.com>
2022-06-08 11:57:14 -0300

    Validação de campos e armazenamento de usuário

--- bait_form/models.py
+++ bait_form/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=100)
+    filledPasswords = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.username} preencheu a senha.'
--- bait_form/views.py
+++ bait_form/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from bait_form.models import Usuario
 
 def index(request):
-    return render(request, 'bait_form/index.html')
+    if request.method == "GET":
+        return render(request, 'bait_form/index.html')
+    else:
+        username = request.POST['username']
+        Usuario.objects.create(username= username)
+        return HttpResponseRedirect('/success/')
 
+def success(request):
+    if request.method == "GET":
+        return render(request, 'bait_form/success.html')
 # Create your views here.
